e52c511001738b9b8eb1545bbb67977441d0a85b
Prokopenko Artsiom <prokopenko.art@mail.ru>
2022-07-01 11:50:27 +0300

    v1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from K8s</h1><br>";
+echo "Server IP: $ip_server", "<br><p>";
+echo "Made by <font color=blue>ART PRO"
+
+?>
+
+
+
+
+
+
+
